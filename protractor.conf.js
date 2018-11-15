e6c187d765a902960ef87884dec48b986f1032bd
Paulo Gonçalves <paulorochag@hotmail.com>
2018-11-15 12:41:53 -0200

    Added dependencies 'prettier' and 'pre-commit'. Executed 'prettier'.

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,19 +1,15 @@
-'use strict'
+"use strict";
 
-const SpecReporter = require('jasmine-spec-reporter').SpecReporter
-const AllureReporter = require('jasmine-allure-reporter');
+const SpecReporter = require("jasmine-spec-reporter").SpecReporter;
+const AllureReporter = require("jasmine-allure-reporter");
 
 module.exports.config = {
-  baseUrl: 'http://homologacao.4yousee.com.br/admin/',
-  specs: ['specs/*.spec.js'],
+  baseUrl: "http://homologacao.4yousee.com.br/admin/",
+  specs: ["specs/*.spec.js"],
   capabilities: {
-    browserName: 'chrome',
+    browserName: "chrome",
     chromeOptions: {
-      args: [
-        '--headless',
-        '--disable-gpu',
-        '--window-size=1024,768'
-      ]
+      args: ["--headless", "--disable-gpu", "--window-size=1024,768"]
     }
   },
   params: {
@@ -23,33 +19,39 @@ module.exports.config = {
     }
   },
   onPrepare: () => {
-    browser.ignoreSynchronization = true
-    jasmine.getEnv().addReporter(new SpecReporter({
-      suite: {
-        displayNumber: true
-      },
-      spec: {
-        displayFailed: true,
-        displayPending: true,
-        displayDuration: true,
-        displayStackTrace: true
-      },
-      summary: {
-        displayFailed: true
-      }
-    }))
-    jasmine.getEnv().addReporter(new AllureReporter());
-    jasmine.getEnv().afterEach(function(done){
-      browser.takeScreenshot().then(function (png) {
-        allure.createAttachment('Screenshot', function () {
-          return new Buffer(png, 'base64')
-        }, 'image/png')();
-        done();
+    browser.ignoreSynchronization = true;
+    jasmine.getEnv().addReporter(
+      new SpecReporter({
+        suite: {
+          displayNumber: true
+        },
+        spec: {
+          displayFailed: true,
+          displayPending: true,
+          displayDuration: true,
+          displayStackTrace: true
+        },
+        summary: {
+          displayFailed: true
+        }
       })
+    );
+    jasmine.getEnv().addReporter(new AllureReporter());
+    jasmine.getEnv().afterEach(function(done) {
+      browser.takeScreenshot().then(function(png) {
+        allure.createAttachment(
+          "Screenshot",
+          function() {
+            return new Buffer(png, "base64");
+          },
+          "image/png"
+        )();
+        done();
+      });
     });
   },
   jasmineNodeOpts: {
     random: true,
     print: function() {}
   }
-}
+};
